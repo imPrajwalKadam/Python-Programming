32ef844409cb86b16a27034c0cdeabdea760850e
imprajwal <atharvak3.2000@gmail.com>
2025-04-23 18:30:31 +0530

    explore nested list in python

---
+++ Python_Concepts/List/nestedList.py
@@ -0,0 +1,25 @@
+# Normal List
+# demoList = ["atharva","prajwal","viraj","swaraj","siddhi"]
+# for name in demoList:
+#           print(name)
+#Nested List
+
+# NestList = [
+#         [1,"Atharva",7020943079],#0
+#         [2,"prajwal",7745832612],#1
+#         [3,"viraj",2020909090],#2
+#         [4,"swaraj",9089899909],#3
+#         [5,"siddhi",2287674453] #4
+# ]
+# for data in NestList:
+#           for i in data:
+#                   print(i,end=" ")
+#           print()
+
+# list2 = [1,[2,3],4,["aths",5],6]
+# print(list2[0]) #1
+# print(list2[1]) #[2,3]
+# print(list2[2]) #4
+# print(list2[3]) #["aths",5]
+# print(list2[4]) #6
+# print(list2[0],list2[3][0])#1 aths
